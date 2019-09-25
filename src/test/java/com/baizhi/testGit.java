f79ab133c8e2ee815edd7fe86abac80e38498712
githubyuan000 <55747978+githubyuan000@users.noreply.github.com>
2019-09-25 16:17:30 +0800

    在远程仓库里添加了一行数据

--- src/test/java/com/baizhi/testGit.java
+++ src/test/java/com/baizhi/testGit.java
@@ -3,5 +3,6 @@ package com.baizhi;
 public class testGit {
     public void test1() {
         System.out.println("======创建一个方法=======");
+        System.out.println("======添加了一行数据=======");
     }
 }
