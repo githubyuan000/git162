c617d0070b7da54a6b272613ddd02fe792f0eb10
yuansp <Yuansp@163.com>
2019-09-25 15:39:05 +0800

    第一次创建项目，第一次提交

---
+++ src/main/java/com/baizhi/Git162Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git162Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git162Application.class, args);
+    }
+
+}
