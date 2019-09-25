6c36b122f68872fbe287d3b83c9d4e1050f6a45f
yuansp <Yuansp@163.com>
2019-09-25 16:01:48 +0800

    在测试类testGit添加了一个方法test1===

--- src/test/java/com/baizhi/testGit.java
+++ src/test/java/com/baizhi/testGit.java
@@ -2,6 +2,6 @@ package com.baizhi;
 
 public class testGit {
     public void test1() {
-        System.out.println("======创建一个方法======");
+        System.out.println("======创建一个方法=======");
     }
 }
